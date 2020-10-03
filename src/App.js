39e0dc7b3e4aa7f223d50f2ae15caca56655fd74
Prayag <prayag.mittal@singtel.com>
2020-10-03 17:33:51 +0800

    api call

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const hitme = () => {
+    fetch("http://192.168.1.124:8070/getDetails")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log("result");
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+        }
+      )
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => hitme()}>Hit me</button>
       </header>
     </div>
   );
